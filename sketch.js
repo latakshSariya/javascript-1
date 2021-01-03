c5610e8aa9a23bf90ed39b2b80de99c9a5e4a484
latakshSariya <72252422+latakshSariya@users.noreply.github.com>
2021-01-03 19:10:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+/* console.clear(); */
+
+/* console.log("this is a example of console.log");
+/* console.clear(); */
+/* console.log(54);
+console.log(["lataksh" , 54 , "learn with lataksh"]); */ 
+
+/* console.error("this is a simple error");
+ */
+
+/*  console.warn("this is a warning"); */
+/* console.log("lataksh");
+console.warn("this is a warning");
+console.error("this is a errror");
+console.clear(); */
+
+/* console.table({'a' :  1 , 'b' : 2}) */
+/*  for(var i=0 ; i<5 ; i = i++){
+    console.count(i);
+}  */
+/* console.group('sipmle');
+console.log("lataksh ");
+console.log("subscribe to lataksh");
+console.groupEnd();
+console.log(214) */
